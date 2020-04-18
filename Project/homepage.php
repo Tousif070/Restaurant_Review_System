f71b41929e685dfaea6c24a59cc2069adaa34865
Tousif Hoque <45402433+Tousif070@users.noreply.github.com>
2020-04-18 19:22:51 +0600

    Footer Module Added Separately

--- Project/homepage.php
+++ Project/homepage.php
@@ -46,27 +46,8 @@
             </div>
         </section>
 
-        <footer>
-            <div class="container">
-                <hr>
-
-                <p class="contact-us">
-                    For any inquiry or to send your feedback<br>
-                    Email us at
-                    <span>eat&rate@business.official.com</span>
-                </p>
-
-                <p class="docs">
-                    <a href="">Terms & Conditions</a> | <a href="">Privacy Policy</a>
-                </p>
-
-                <p class="copyright">
-                    © 2020 Eat&Rate.com . All Rights Reserved
-                </p>
-
-                <hr class="bottomline">
-            </div>
-        </footer>
+        <!-- FOOTER INCLUDED -->
+        <?php include "footer.php" ?>
 
     </body>
 
